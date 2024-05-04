d0e3add6341aa44cd17b681b562e10298e44cb47
ckopf <C.Kopf.ext@funk-gruppe.de>
2024-05-04 12:15:57 +0200

    improve readability

--- ChurnR/Program.cs
+++ ChurnR/Program.cs
@@ -5,24 +5,20 @@ using CommandLine;
 using Microsoft.Extensions.DependencyInjection;
 using Serilog;
 
+var services = new ServiceCollection();
+
 var parserResult = Parser.Default.ParseArguments<GitOptions, SvnOptions>(args);
-
-parserResult.WithNotParsed(_ =>
-{
-    Exit(ExitCode.Parameters);
-});
-
-var services = new ServiceCollection(); 
-
+parserResult.WithNotParsed(_ => Exit(ExitCode.Parameters));
 parserResult.WithParsed<GitOptions>(gitOptions => services.AddChurnR(gitOptions));
 parserResult.WithParsed<SvnOptions>(svnOptions => services.AddChurnR(svnOptions));
 
 await using var provider = services.BuildServiceProvider();
 using var serviceScope = provider.CreateScope();
+
 var engine = serviceScope.ServiceProvider.GetRequiredService<Engine>();
+var result = engine.Run();
 
-Exit(engine.Run());
-
+Exit(result);
 
 void Exit(ExitCode exitCode, string? message = null)
 {
