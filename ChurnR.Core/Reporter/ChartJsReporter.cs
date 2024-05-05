ca60a5f69c307122662cd19411477c4dffa91df6
ckopf <C.Kopf.ext@funk-gruppe.de>
2024-05-05 20:04:36 +0200

    Move template to file

--- ChurnR.Core/Reporter/ChartJsReporter.cs
+++ ChurnR.Core/Reporter/ChartJsReporter.cs
@@ -9,6 +9,8 @@ public class ChartJsReporter(ILogger logger, TextWriter output, IProcessor cutOf
 {
     protected override void WriteImpl(IEnumerable<FileStatistics> fileStatistics)
     {
+        var htmlTemplate = File.ReadAllText("Reporter/Templates/ChurnR_ChartJs_Report_Template.html");
+        
         Logger.Information("Generating ChartJs report");
         var churns = fileStatistics
             .Select(file => new
@@ -22,97 +24,20 @@ public class ChartJsReporter(ILogger logger, TextWriter output, IProcessor cutOf
                 FileNameCount = file.HistoricFullFileNames.Count
             }).ToList();
         
-        commitCount = string.Join(",", churns.OrderByDescending(x => x.CommitCount).Select(x => "{" + $"x:'{x.UniqueFileName}',y:{x.CommitCount}" + "}"));
-        lineChurns = string.Join(",", churns.OrderByDescending(x => x.TotalLineChurns).Select(x => "{" + $"x:'{x.UniqueFileName}',y:{x.TotalLineChurns}" + "}"));
-        averageChurnsPerCommit = string.Join(",", churns.OrderByDescending(x => x.AverageLineChurnsPerCommit).Select(x => "{" + $"x:'{x.UniqueFileName}',y:{x.AverageLineChurnsPerCommit.ToString(CultureInfo.InvariantCulture)}" + "}"));
-        fileNameCount = string.Join(",", churns.OrderByDescending(x => x.FileNameCount).Select(x => "{" + $"x:'{x.UniqueFileName}',y:{x.FileNameCount}" + "}"));
+        var commitsPerFile = string.Join(",", churns.OrderByDescending(x => x.CommitCount).Select(x => "{" + $"x:'{x.UniqueFileName}',y:{x.CommitCount}" + "}"));
+        var totalLineChurnsPerFile = string.Join(",", churns.OrderByDescending(x => x.TotalLineChurns).Select(x => "{" + $"x:'{x.UniqueFileName}',y:{x.TotalLineChurns}" + "}"));
+        var averageChurnPerCommitPerFile = string.Join(",", churns.OrderByDescending(x => x.AverageLineChurnsPerCommit).Select(x => "{" + $"x:'{x.UniqueFileName}',y:{x.AverageLineChurnsPerCommit.ToString(CultureInfo.InvariantCulture)}" + "}"));
+        var renamesOrMovesPerFile = string.Join(",", churns.OrderByDescending(x => x.FileNameCount).Select(x => "{" + $"x:'{x.UniqueFileName}',y:{x.FileNameCount}" + "}"));
+
+        htmlTemplate = htmlTemplate
+            .Replace("{commitsPerFile}", commitsPerFile)
+            .Replace("{totalLineChurnsPerFile}", totalLineChurnsPerFile)
+            .Replace("{averageChurnPerCommitPerFile}", averageChurnPerCommitPerFile)
+            .Replace("{renamesOrMovesPerFile}", renamesOrMovesPerFile);
+
+        Logger.Debug(htmlTemplate);
         
-        Logger.Debug(HtmlTemplate);
-        
-        Out.Write(HtmlTemplate);
+        Out.Write(htmlTemplate);
     }
-
-    private static string commitCount = "";
-    private static string lineChurns = "";
-    private static string averageChurnsPerCommit = "";
-    private static string fileNameCount = "";
-    
-    private string HtmlTemplate => @"
-        <!DOCTYPE html>
-        <html lang=""en"">
-           <body>
-              <div>
-                 <canvas id=""commitCount""></canvas>
-              </div>
-              <div>
-                 <canvas id=""lineChurns""></canvas>
-              </div>
-              <div>
-                 <canvas id=""averageChurnsPerCommit""></canvas>
-              </div>
-              <div>
-                 <canvas id=""fileNameCount""></canvas>
-              </div>
-              <script src=""https://cdn.jsdelivr.net/npm/chart.js""></script>
-              <script>
-                 new Chart(document.getElementById('commitCount'), {
-                   type: 'line',
-                   data: {
-                     datasets: 
-                     [{
-                         label: 'Involved in Commits',
-                         data: ["+commitCount+@"],
-                         fill: false,
-                         borderColor: 'rgb(75, 192, 192)',
-                         tension: 0.1
-                       }
-                     ]}
-                 });
-
-                 new Chart(document.getElementById('lineChurns'), {
-                   type: 'line',
-                   data: {
-                     datasets: 
-                     [{
-                         label: 'Total lines added and deleted',
-                         data: ["+lineChurns+@"],
-                         fill: false,
-                         borderColor: 'rgb(192, 75, 192)',
-                         tension: 0.1
-                       }
-                     ]}
-                 });
-
-                 new Chart(document.getElementById('averageChurnsPerCommit'), {
-                   type: 'line',
-                   data: {
-                     datasets: 
-                     [{
-                         label: 'Average Churns / Commit',
-                         data: ["+averageChurnsPerCommit+@"],
-                         fill: false,
-                         borderColor: 'rgb(192, 192, 75)',
-                         tension: 0.1
-                       }
-                     ]}
-                 });
-
-                 new Chart(document.getElementById('fileNameCount'), {
-                   type: 'line',
-                   data: {
-                     datasets: 
-                     [{
-                         label: 'Rename or Move Count',
-                         data: ["+fileNameCount+@"],
-                         fill: false,
-                         borderColor: 'rgb(75, 156, 215)',
-                         tension: 0.1
-                       }
-                     ]}
-                 });
-              </script>
-           </body>
-        </html>
-        ";
 }
 
